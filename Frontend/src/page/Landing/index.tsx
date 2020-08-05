3631c27460bad5823314be483d38f511a516f4b6
Italo Guglielmini <italo.guglielmini@gmail.com>
2020-08-05 15:24:09 -0300

    ajustes de routes

--- Frontend/src/page/Landing/index.tsx
+++ Frontend/src/page/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import LogoImg from '../../assets/images/logo.svg';
 import LandingImg from '../../assets/images/landing.svg';
 import StudyIcons from '../../assets/images/icons/study.svg';
@@ -26,21 +26,21 @@ function Landing() {
 
                 <div className="buttons-container">
 
-                    <a href="/" className="study">
+                    <Link to="/study" className="study">
                         <img src={StudyIcons} alt="Estudar" />
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="/" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={GiveClassesIcon} alt="Estudar" />
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizada
                     <img
                         src={PurpleHeartIcon}
-                        alt="Coração Roxo" 
+                        alt="Coração Roxo"
                     />
                 </span>
             </div>

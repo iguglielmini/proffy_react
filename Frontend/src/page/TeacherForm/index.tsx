0e42ceca1a42326bb665d4ec648e53cd6f1614d9
Italo Guglielmini <italo.guglielmini@gmail.com>
2020-08-05 15:20:53 -0300

    ajustes de Routes

---
+++ Frontend/src/page/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+
+//css
+import 'styles.css'
+
+function TeacherForm(){
+    return(
+        <h1>Teachers</h1>
+    )
+}
+
+export default TeacherForm;
---
+++ Frontend/src/page/TeacherList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+
+//css
+import 'styles.css'
+
+function TeacherList(){
+    return(
+        <h1>Teachers</h1>
+    )
+}
+
+export default TeacherList;
